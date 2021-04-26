078e7c20041758c7c0c929b2fe0e909fe4e552af
Jon Marquis <jon.marquis@gmail.com>
2021-04-26 09:11:01 -0600

    data import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import PokemonList from './components/PokemonList';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  // eslint-disable-next-line
   const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p=>p.name))
+  })
   return (
     <PokemonList pokemon= {pokemon} />
   );
